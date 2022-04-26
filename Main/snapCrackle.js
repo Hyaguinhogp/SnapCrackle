ee8d609275288ba515db71df7b8210431f426ec3
Hyaguinhogp <hyago.v.a.m@gmail.com>
2022-04-26 09:37:36 -0400

    SnapCracklePrime

--- Main/snapCrackle.js
+++ Main/snapCrackle.js
@@ -8,3 +8,30 @@ function snapCrackle(maxValue){
     }
     return str;
 }
+
+function snapCracklePrime(maxValue){
+    let str = '';
+    for(let i = 1; i <= maxValue; i++){
+        if( i % 2 == 0){
+            if ( i != 2 ) {
+                if( i % 5 == 0) {
+                    str += 'Crackle'
+                } else str += i;
+            }
+        } else {
+            str += 'Snap';
+            if( i % 5 == 0) str += 'Crackle';
+        }
+
+
+        let divisorCount = 0;
+        for(let j = 1; j <= i; j++){
+            if( i % j == 0) divisorCount++;
+        }
+        if(divisorCount == 2) str += 'Prime';
+        divisorCount = 0;
+
+        if( i < maxValue ) str += ', ';
+    }
+    return str;
+}
